1cef0a4cc4efb26363db2aa832630dcbf690464d
Daniel K. Valente <dan.k.valente@gmail.com>
2023-12-21 16:12:03 -1000

    - Added documentation.

--- NetworkIP.cs
+++ NetworkIP.cs
@@ -3,30 +3,42 @@ using System.Net.NetworkInformation;
 
 namespace VNetworks
 {
+	/// <summary>
+	/// Provides additional IPAddress information while also maintaining a conversion with the <see cref="IPAddress"/> class.
+	/// </summary>
 	public class NetworkIP:IPAddress
 	{
-
+		/// <summary>
+		/// The <see cref="IPAddress"/> object.
+		/// </summary>
 		public IPAddress Value => new(base.GetAddressBytes());
-
+		/// <summary>
+		/// Gets the active network interface.
+		/// </summary>
 		public NetworkInterface? ActiveNetworkInterface => NetworkInformation.GetNetworkInterfaceFromIPAddress(Value);
-
+		/// <summary>
+		/// Gets the active network information object.
+		/// </summary>
 		public NetworkInformation? ActiveNetworkInformation => ActiveNetworkInterface is null ? null : ((NetworkInformation)ActiveNetworkInterface);
-
+		/// <summary>
+		/// Gets the subnetmask of the current IPAddress.
+		/// </summary>
 		public IPAddress? SubnetMask => ActiveNetworkInformation?.IPv4Mask;
 
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(byte[] address) : base(address)
 		{
 		}
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(long newAddress) : base(newAddress)
 		{
 		}
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(ReadOnlySpan<byte> address) : base(address)
 		{
 		}
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(byte[] address, long scopeid) : base(address, scopeid)
 		{
 		}
@@ -38,11 +50,11 @@ namespace VNetworks
 		public NetworkIP(ReadOnlySpan<byte> address, long scopeid) : base(address, scopeid)
 		{
 		}
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(IPAddress address) : base(address.GetAddressBytes())
 		{
 		}
-		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long}"/>
+		/// <inheritdoc cref="NetworkIP(ReadOnlySpan{byte}, long)"/>
 		public NetworkIP(string address) : this(Parse(address))
 		{
 		}
